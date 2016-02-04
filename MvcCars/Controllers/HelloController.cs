f93199cbf159f797120ccd66e3f98b8dfed4b5fc
Michał D <michcio1234@gmail.com>
2016-02-04 20:35:42 +0100

    Init

---
+++ MvcCars/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCars.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public string Index()
+        {
+            return "Akcja <b>domyślna</b>.";
+        }
+
+        public string Welcome(string name, int id = 1)
+        {
+            return HttpUtility.HtmlEncode("Witaj " + name + ", numer wizyty: " + id);
+        }
+    }
+}
---
+++ MvcCars/Controllers/MarianController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCars.Controllers
+{
+    public class MarianController : Controller
+    {
+        // GET: Marian
+        public string Index()
+        {
+            return "Eloszka, jestem Marian.";
+        }
+
+        public string Jump()
+        {
+            return "Podskoczyłem sobie. Hopsasa.";
+        }
+    }
+}
